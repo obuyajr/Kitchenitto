a3ff43e97a2226e0d59f479e91aab71ee5c01dd8
obuyajr <felix37brock@gmail.com>
2023-03-31 02:48:44 +0300

    singup-login12

--- app/src/main/java/com/example/kitchenitto/Login.java
+++ app/src/main/java/com/example/kitchenitto/Login.java
@@ -1,14 +1,61 @@
 package com.example.kitchenitto;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
 
+    private EditText mEmailEditText;
+    private EditText mPasswordEditText;
+    private Button mLoginButton;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mEmailEditText = findViewById(R.id.email_edit_text);
+        mPasswordEditText = findViewById(R.id.password_edit_text);
+        mLoginButton = findViewById(R.id.login_button);
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Handle login button click here
+                String email = mEmailEditText.getText().toString();
+                String password = mPasswordEditText.getText().toString();
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    // Handle successful login here
+                                } else {
+                                    // Sign in failed
+                                    Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+
+            }
+        });
     }
 }
