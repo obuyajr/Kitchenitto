b8755d4f78f981a01cf008b7b7bf740bfcd46307
obuyajr <felix37brock@gmail.com>
2023-03-31 02:10:58 +0300

    singup-login

--- app/src/main/java/com/example/kitchenitto/MainActivity.java
+++ app/src/main/java/com/example/kitchenitto/MainActivity.java
@@ -6,9 +6,37 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText signupEmail, signupPassword, loginEmail, loginPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signupEmail = findViewById(R.id.signup_email);
+        signupPassword = findViewById(R.id.signup_password);
+        Button signupButton = findViewById(R.id.signup_button);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = signupEmail.getText().toString();
+                String password = signupPassword.getText().toString();
+                // Add code to sign up user
+            }
+        });
+
+        loginEmail = findViewById(R.id.login_email);
+        loginPassword = findViewById(R.id.login_password);
+        Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = loginEmail.getText().toString();
+                String password = loginPassword.getText().toString();
+                // Add code to log in user
+            }
+        });
+    }
+}
     }
 }
