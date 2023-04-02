e2209f80e7d254b520e3a83c62fa1fcd35988cf9
obuyajr <felix37brock@gmail.com>
2023-04-02 14:16:12 +0300

    singup-log

--- app/src/main/java/com/example/kitchenitto/Signup.java
+++ app/src/main/java/com/example/kitchenitto/Signup.java
@@ -3,12 +3,18 @@ package com.example.kitchenitto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class Signup extends AppCompatActivity {
+    TextInputEditText emailTxt,passwordTxt;
+    Button login_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
     }
 }
